f5299c6ad3aed70f56abc2051e35a79a3097edb4
FedericoCostantini <federico.costantini@polimi.it>
2023-07-19 00:31:43 +0200

    final commit

--- NNv2.py
+++ NNv2.py
@@ -36,39 +36,30 @@ def convert(x: int) -> np.ndarray:
     return arr
 
 @jit(nopython=True)
-def _forward_propagation(x: np.ndarray, h1: np.ndarray, h2: np.ndarray, o: np.ndarray, weightIH1: np.ndarray, 
-                         weightH1H2: np.ndarray, weightH2O: np.ndarray, biasH1: np.ndarray, biasH2: np.ndarray, biasO: np.ndarray) -> None:
+def _forward_propagation(x: np.ndarray, h1: np.ndarray, o: np.ndarray, weightIH1: np.ndarray, 
+                         weightH2O: np.ndarray, biasH1: np.ndarray, biasO: np.ndarray) -> None:
     h1 += np.dot(weightIH1, x) + biasH1
     h1 *= (h1 > 0)
     
-    h2 += np.dot(weightH1H2, h1) + biasH2
-    h2 *= (h2 > 0)
-    
-    o += softmax(np.dot(weightH2O, h2) + biasO)
+    o += softmax(np.dot(weightH2O, h1) + biasO)
     
 @jit(nopython=True)
-def _backward_propagation(inp: np.ndarray, h1: np.ndarray, h2: np.ndarray, o: np.ndarray, exp: np.ndarray, weightIH1: np.ndarray,
-                          weightH1H2: np.ndarray, weightH2O: np.ndarray, biasH1: np.ndarray, biasH2: np.ndarray, biasO: np.ndarray, lr: float) -> None:
+def _backward_propagation(inp: np.ndarray, h1: np.ndarray, o: np.ndarray, exp: np.ndarray, weightIH1: np.ndarray, 
+                          weightH2O: np.ndarray, biasH1: np.ndarray, biasO: np.ndarray, lr: float) -> None:
     dBO = cross_entropy_derivative(o, exp) * softmax_derivative(o)
-    dWH2O = np.dot(dBO, h2.transpose())
+    dWH2O = np.dot(dBO, h1.transpose())
     
-    dB2 = np.dot(weightH2O.transpose(), dBO) * (h2 > 0)
-    dWH1H2 = np.dot(dB2, h1.transpose())
-    
-    dB1 = np.dot(weightH1H2.transpose(), dB2) * (h1 > 0)
+    dB1 = np.dot(weightH2O.transpose(), dBO) * (h1 > 0)
     dWIH1 = np.dot(dB1, inp.transpose())
     
     weightIH1 -= dWIH1 * lr
     biasH1 -= dB1 * lr
     
-    weightH1H2 -= dWH1H2 * lr
-    biasH2 -= dB2 * lr
-    
     weightH2O -= dWH2O * lr
     biasO -= dBO * lr   
 
 class NeuralNetwork:
-    def __init__(self, toLoad: bool, ep: int = 100, batch: int = 10000, lr: float = 0.01, num_h1: int = 32, num_h2: int = 32, num_o: int = 10) -> None:
+    def __init__(self, toLoad: bool, ep: int = 100, batch: int = 10000, lr: float = 0.01, num_h1: int = 128, num_o: int = 10) -> None:
         np.random.seed(7)
         script_path = os.path.abspath(__file__)
         self.directory = os.path.dirname(script_path)
@@ -87,24 +78,19 @@ class NeuralNetwork:
         
         if toLoad:
             self.weightIH1 = np.load(self.directory + "/weightIH1.npy")
-            self.weightH1H2 = np.load(self.directory + "/weightH1H2.npy")
             self.weightH2O = np.load(self.directory + "/weightH2O.npy")
             
             self.biasH1 = np.load(self.directory + "/biasH1.npy")
-            self.biasH2 = np.load(self.directory + "/biasH2.npy")
             self.biasO = np.load(self.directory + "/biasO.npy")
             
         else:
             self.weightIH1 = np.random.random((num_h1, 784)).astype(np.float64) - 0.5
-            self.weightH1H2 = np.random.random((num_h2, num_h1)).astype(np.float64) - 0.5
-            self.weightH2O = np.random.random((num_o, num_h2)).astype(np.float64) - 0.5
+            self.weightH2O = np.random.random((num_o, num_h1)).astype(np.float64) - 0.5
             
             self.biasH1 = np.random.random((num_h1,1)).astype(np.float64) / 10
-            self.biasH2 = np.random.random((num_h2,1)).astype(np.float64) / 10
             self.biasO = np.random.random((num_o,1)).astype(np.float64) / 10
         
         self.h1 = np.zeros((num_h1,1)).astype(np.float64)
-        self.h2 = np.zeros((num_h2,1)).astype(np.float64)
         self.o = np.zeros((num_o,1)).astype(np.float64)
         
         self.ep = ep
@@ -139,35 +125,32 @@ class NeuralNetwork:
             for i in range(self.batch):
                 inp : np.ndarray = np.copy(imgs[index])
                 inp[inp == 0] = 0.0001
-                _forward_propagation(inp, self.h1, self.h2, self.o, self.weightIH1, self.weightH1H2, self.weightH2O, self.biasH1, self.biasH2, self.biasO)
+                _forward_propagation(inp, self.h1, self.o, self.weightIH1, self.weightH2O, self.biasH1, self.biasO)
                 
-                _backward_propagation(inp, self.h1, self.h2, self.o, convert(labels[index][0]), self.weightIH1, self.weightH1H2, self.weightH2O, self.biasH1, self.biasH2, self.biasO, self.lr)
+                _backward_propagation(inp, self.h1, self.o, convert(labels[index][0]), self.weightIH1, self.weightH2O, self.biasH1, self.biasO, self.lr)
                 
                 self.o = np.zeros_like(self.o)
                 self.h1 = np.zeros_like(self.h1)
-                self.h2 = np.zeros_like(self.h2)
                 
                 index += 1
                 if(index == self.vector_images.shape[0]):
                     break
             
             if epoch == self.ep // 2:
-                self.lr /= 2
+                self.lr /= 4
             
             print("epoch {0} percentage test {1}".format(epoch + 1, self.evaluate()))
         
         np.save(self.directory + "/weightIH1.npy", self.weightIH1)
-        np.save(self.directory + "/weightH1H2.npy", self.weightH1H2)
         np.save(self.directory + "/weightH2O.npy", self.weightH2O)
         
         np.save(self.directory + "/biasH1.npy", self.biasH1)
-        np.save(self.directory + "/biasH2.npy", self.biasH2)
         np.save(self.directory + "/biasO.npy", self.biasO)
     
     def overfit(self) -> float:
         correct= 0
         for i in range(self.vector_images.shape[0]):
-            _forward_propagation(self.vector_images[i], self.h1, self.h2, self.o, self.weightIH1, self.weightH1H2, self.weightH2O, self.biasH1, self.biasH2, self.biasO)
+            _forward_propagation(self.vector_images[i], self.h1, self.o, self.weightIH1, self.weightH2O, self.biasH1, self.biasO)
             ans = self.get_ans()
             
             if ans == self.train_labels[i][0]:
@@ -175,14 +158,13 @@ class NeuralNetwork:
         
             self.o = np.zeros_like(self.o)
             self.h1 = np.zeros_like(self.h1)
-            self.h2 = np.zeros_like(self.h2)
         
         return (correct / self.vector_images.shape[0]) * 100
     
     def evaluate(self) -> float:
         correct= 0
         for i in range(self.test_images.shape[0]):
-            _forward_propagation(self.test_images[i], self.h1, self.h2, self.o, self.weightIH1, self.weightH1H2, self.weightH2O, self.biasH1, self.biasH2, self.biasO)
+            _forward_propagation(self.test_images[i], self.h1, self.o, self.weightIH1, self.weightH2O, self.biasH1, self.biasO)
             ans = self.get_ans()
             
             if ans == self.test_labels[i][0]:
@@ -190,7 +172,6 @@ class NeuralNetwork:
         
             self.o = np.zeros_like(self.o)
             self.h1 = np.zeros_like(self.h1)
-            self.h2 = np.zeros_like(self.h2)
         
         return (correct / self.test_images.shape[0]) * 100
     
@@ -198,16 +179,15 @@ class NeuralNetwork:
         inp = np.reshape(inp, (784, 1)).astype(np.float64)
         mod_inp = np.copy(inp)
         mod_inp[mod_inp == 0] = 0.0001
-        _forward_propagation(mod_inp, self.h1, self.h2, self.o, self.weightIH1, self.weightH1H2, self.weightH2O, self.biasH1, self.biasH2, self.biasO)
+        _forward_propagation(mod_inp, self.h1, self.o, self.weightIH1, self.weightH2O, self.biasH1, self.biasO)
         ans = self.get_ans()
         
         self.o = np.zeros_like(self.o)
         self.h1 = np.zeros_like(self.h1)
-        self.h2 = np.zeros_like(self.h2)
         
         print("ARI guess it's a {0}".format(ans))
             
  
 if __name__ == "__main__":
-    nn = NeuralNetwork(False, 100, 10000, 0.01, 32, 32, 10)
+    nn = NeuralNetwork(False, 40, 10000, 0.01, 128, 10)
     nn.train()
--- README.md
+++ README.md
@@ -17,3 +17,15 @@ The first version (file NNv1.py) uses the simpliest scenario. A neural network c
 * Activation function for hidden layers: sigmoid
 * Activation function for output: sigmoid
 * Test precision: 96.52%
+
+## Version 2
+The second version (file NNv2.py) uses a neural network consisting of a single hidden layer of 128 nodes
+* Cost function: cross-entropy
+* Activation function for hidden layer: ReLU
+* Activation function for output: softmax
+* Test precision: 97.61%
+
+I managed to get up to 97.71% test accuracy if I let the program train for 100 epoch, but it takes too much time for just a 0.1% improvement
+
+## Manual test
+I added a pygame script that let's the user draw a number using the mouse. Press a to clean the board, press s to make the neural network guess the written digit. It's hard for it to recognise every type of handwriting style since the MNIST dataset has a very small variety of handwriting styles
